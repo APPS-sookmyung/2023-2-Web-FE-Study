f3d8ffa69c913006019730520990d98196b2b50e
codeJiwon <loopy0107@sookmyung.ac.kr>
2023-11-03 00:16:54 +0900

    Feat: 4주차 과제 MenuApi 객체 분리

--- 정지원/moonbucks-menu/src/js/index.js
+++ 정지원/moonbucks-menu/src/js/index.js
@@ -1,6 +1,65 @@
 import { $ } from "//utils/dom.js";
 import store from "./store/index.js";
 
+const BASE_URL = "http://localhost:3000/api";
+
+const MenuApi = {
+  async getAllMenuByCategory(category) {
+    const response = await fetch("${BASE_URL}/category/${category}/menu");
+    return response.json();
+  },
+  async createMenu(category, name) {
+    const response = await fetch("${BASE_URL}/category/${category}/menu", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", //주고받는 컨텐츠 타입을 정의할 수 있는데 대부분 json 사용
+      },
+      body: JSON.stringify({ name }), //사용자가 입력한 값을 넣어서 서버에 요청
+    });
+    if (resopnse.ok) {
+      console.error("에러가 발생했습니다.");
+    }
+  },
+  async updateMenu(category, name, menuId) {
+    const response = await fetch(
+      "${BASE_URL}/category/${category}/menu/${menuId}",
+      {
+        method: "PUT", //생성할 땐 PUT을 사용한다는 것만 알아두기
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      }
+    );
+    if (resopnse.ok) {
+      console.error("에러가 발생했습니다.");
+    }
+    return response.json();
+  },
+  async toggleSoldOutMenu(category, menuId) {
+    const response = await fetch(
+      "${BASE_URL}/category/${category}/menu/${menuId}/soldout",
+      {
+        method: "PUT",
+      }
+    );
+    if (resopnse.ok) {
+      console.error("에러가 발생했습니다.");
+    }
+  },
+  async deleteMenu(category, menuId) {
+    const response = await fetch(
+      "${BASE_URL}/category/${category}/menu/${menuId}",
+      {
+        method: "DELETE",
+      }
+    );
+    if (resopnse.ok) {
+      console.error("에러가 발생했습니다.");
+    }
+  },
+};
+
 function App() {
   //상태 - 메뉴명
   this.menu = {
@@ -10,11 +69,12 @@ function App() {
     tevana: [],
     desert: [],
   };
-  this.currentCategory = "";
-  this.init = () => {
-    if (store.getlocalStorage()) {
-      this.menu = store.getlocalStorage();
-    }
+  this.currentCategory = "espresso";
+
+  this.init = async () => {
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      currentCategory
+    );
     render();
     initEventListeners();
   };
@@ -23,9 +83,11 @@ function App() {
     const template = this.menu[this.currentCategory]
       .map((menuItem, index) => {
         return `
-        <li data-menu-id="${index}" class= "menu-list-item d-flex items-center py-2">
+        <li data-menu-id="${
+          menuItem.id
+        }" class= "menu-list-item d-flex items-center py-2">
           <span class="w-100 pl-2 menu-name ${
-            menuItem.soldOut ? "sold-out" : ""
+            menuItem.isSoldOut ? "sold-out" : ""
           } ">${menuItem.name}</span>
           <button
             type="button"
@@ -61,45 +123,50 @@ function App() {
   };
 
   //중복되는 코드를 재사용할 수 있도록하는 코드
-  const addMenuName = () => {
+  const addMenuName = async () => {
     if ($("#menu-name").value === "") {
       alert("값을 입력해주세요");
       return;
     }
-    if (e.key === "Enter") {
-      const MenuName = $("menu-name").value;
-      this.menu[this.currentCategory].push({ name: menuName });
-      store.setlocalStorage(this.menu);
-      render();
-      $("#menu-name").value = "";
-    }
+    const MenuName = $("menu-name").value;
+    await MenuApi.createMenu(this.currentCategory, menuName);
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      //여기서는 this가 App 안에서 사용되기 때문에 this.currentCategory로 불러옴
+      this.currentCategory
+    );
+    render();
+    $("menu-name").value = "";
   };
 
-  const updatedMenuName = (e) => {
+  const updatedMenuName = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
     //중복되는 부분 변수에 담아서 깔끔하게 사용
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updatedMenuName = prompt("메뉴명을 수정하세요", menuName, innerText);
-    this.menu[this.currentCategory][menuId].name = updatedMenuName;
-    storage.localStorage.setItem(this.menu);
+    await MenuApi.updateMenu(this.currentCategory, updatedMenuName, menuId);
+    //this로 App()의 category를 계속 가져올 수 있음
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
     render();
   };
 
-  const removeMenuName = (e) => {
+  const removeMenuName = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
-    this.menu[this.currentCategory].splice(menuId, 1);
-    store.setLocalStorage(this.menu);
-    //li태그를 통으로 삭제해야함
+    await MenuApi.deleteMenu(this.currentCategory, menuId);
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
     render();
     //삭제했을 때 총 개수 -1되도록 함.
   };
 
-  const soldOutMenu = (e) => {
+  const soldOutMenu = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
-
-    this.menu[this.currentCategory][menuId].soldOut =
-      !this.menu[this.currentCategory][menuId].soldOut;
-    store.setLocalStorage(this.menu);
+    await MenuApi.toggleSoldOutMenu(this.currentCategory, this.menuId);
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
     render();
   };
 
@@ -143,13 +210,19 @@ function App() {
         addMenuName();
       });
 
-    $("nav").addEventListener("click", (e) => {
+    $("nav").addEventListener("click", async (e) => {
+      //async는 함수가 시작하는 부분 앞에 작성
       const isCategoryButton =
         e.target.classList.contains("cafe-category-name");
       if (isCategoryButton) {
         const categoryName = e.target.dataset.categoryName;
         this.currentCategory = categoryName;
         $("#category-title").innerText = "${e.target.innerText} 메뉴 관리";
+        //데이터 불러오는 로직을 추가해야 새로고침해도 리스트가 남아있음
+        this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+          this.currentCategory
+        );
+        render();
       }
     });
   };
