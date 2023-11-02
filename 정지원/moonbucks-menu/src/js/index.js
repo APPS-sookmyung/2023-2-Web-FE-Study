088e192d02dbb7d5fa986e04b7ff8687ada2f849
codeJiwon <loopy0107@sookmyung.ac.kr>
2023-11-02 16:59:25 +0900

    Feat: 4주차 과제 웹 서버 요청

--- 정지원/moonbucks-menu/src/js/index.js
+++ 정지원/moonbucks-menu/src/js/index.js
@@ -1,13 +1,5 @@
-const $ = (selector) => document.querySelector(selector); //코드를 줄이기 위함.
-
-const store = {
-  setlocalStorage(menu) {
-    localStorage.setItem("menu".JSON.stringify(menu));
-  },
-  getlocalStorage() {
-    return JSON.parse(localStorage.getItem("menu"));
-  },
-};
+import { $ } from "//utils/dom.js";
+import store from "./store/index.js";
 
 function App() {
   //상태 - 메뉴명
@@ -24,6 +16,7 @@ function App() {
       this.menu = store.getlocalStorage();
     }
     render();
+    initEventListeners();
   };
 
   const render = () => {
@@ -63,8 +56,8 @@ function App() {
   };
 
   const updateMenuCount = () => {
-    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-    $(".menu-count").innerText = "총 ${} 개";
+    const menuCount = this.menu[this.currentCategory].length;
+    $(".menu-count").innerText = "총 ${menuCount} 개";
   };
 
   //중복되는 코드를 재사용할 수 있도록하는 코드
@@ -89,7 +82,7 @@ function App() {
     const updatedMenuName = prompt("메뉴명을 수정하세요", menuName, innerText);
     this.menu[this.currentCategory][menuId].name = updatedMenuName;
     storage.localStorage.setItem(this.menu);
-    $menuName.innerText = updatedMenuName;
+    render();
   };
 
   const removeMenuName = (e) => {
@@ -97,9 +90,8 @@ function App() {
     this.menu[this.currentCategory].splice(menuId, 1);
     store.setLocalStorage(this.menu);
     //li태그를 통으로 삭제해야함
-    e.target.closest("li").remove();
+    render();
     //삭제했을 때 총 개수 -1되도록 함.
-    updateMenuCount();
   };
 
   const soldOutMenu = (e) => {
@@ -111,53 +103,56 @@ function App() {
     render();
   };
 
-  //메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다
-  //모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메유가 수정된다.
-  $("#menu-list").addEventListener("click", (e) => {
-    if (e.target.classList.contains("menu-edit-button")) {
-      updatedMenuName(e);
-      return;
-    }
-
-    //메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 confirm 모달창이 뜬다
-    if (e.target.classList.contains("menu-remove-button")) {
-      removeMenuName(e);
-      return;
-    }
-
-    if (e.target.classList.contains("menu-sold-out-button")) {
-      soldOutMenu(e);
-      return;
-    }
-  });
-
-  //form태그가 자동으로 전송되는 걸 막아준다.(사용자가 입력하 값이 잘 출력되도록 함)
-  $("menu-form") //element 찾기
-    .addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
-
-  $("#menu-submit-button").addEventListener("click", addMenuName);
-
-  //메뉴 이름 입력받고 엔터키 입력으로 추가한다.
-  $("menu-name") //element 찾기
-    .addEventListener("keypress", (e) => {
-      //사용자가 keypress에 입력한 값 받기
-      //빈 값을 입력헀을 때 저장되지 않고 alert메세지 뜨도록 하기
-      if (e.key !== "Enter") {
+  const initEventListeners = () => {
+    //메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다
+    //모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메유가 수정된다.
+    $("#menu-list").addEventListener("click", (e) => {
+      if (e.target.classList.contains("menu-edit-button")) {
+        updatedMenuName(e);
         return;
       }
-      addMenuName();
+
+      //메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 confirm 모달창이 뜬다
+      if (e.target.classList.contains("menu-remove-button")) {
+        removeMenuName(e);
+        return;
+      }
+
+      if (e.target.classList.contains("menu-sold-out-button")) {
+        soldOutMenu(e);
+        return;
+      }
     });
 
-  $("nav").addEventListener("click", (e) => {
-    const isCategoryButton = e.target.classList.contains("cafe-category-name");
-    if (isCategoryButton) {
-      const categoryName = e.target.dataset.categoryName;
-      this.currentCategory = categoryName;
-      $("#category-title").innerText = "${e.target.innerText} 메뉴 관리";
-    }
-  });
+    //form태그가 자동으로 전송되는 걸 막아준다.(사용자가 입력하 값이 잘 출력되도록 함)
+    $("menu-form") //element 찾기
+      .addEventListener("submit", (e) => {
+        e.preventDefault();
+      });
+
+    $("#menu-submit-button").addEventListener("click", addMenuName);
+
+    //메뉴 이름 입력받고 엔터키 입력으로 추가한다.
+    $("menu-name") //element 찾기
+      .addEventListener("keypress", (e) => {
+        //사용자가 keypress에 입력한 값 받기
+        //빈 값을 입력헀을 때 저장되지 않고 alert메세지 뜨도록 하기
+        if (e.key !== "Enter") {
+          return;
+        }
+        addMenuName();
+      });
+
+    $("nav").addEventListener("click", (e) => {
+      const isCategoryButton =
+        e.target.classList.contains("cafe-category-name");
+      if (isCategoryButton) {
+        const categoryName = e.target.dataset.categoryName;
+        this.currentCategory = categoryName;
+        $("#category-title").innerText = "${e.target.innerText} 메뉴 관리";
+      }
+    });
+  };
 }
 
 const app = new App();
